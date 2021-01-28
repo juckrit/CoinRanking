e6d9c9a9b796d4c0670002242aaf5657905b0864
sam <juckrit072@gmail.com>
2021-01-28 23:30:44 +0700

    add mainFragment and nav_graph

---
+++ app/src/main/java/com/example/coinranking/presentation/main/MainFragment.kt
@@ -0,0 +1,22 @@
+package com.example.coinranking.presentation.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.coinranking.R
+
+class MainFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+}
