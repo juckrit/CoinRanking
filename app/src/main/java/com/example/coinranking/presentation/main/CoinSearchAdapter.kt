33e8a37e511e426bad9c44eca712035365be1df5
Juckrit <juckrit072@gmail.com>
2021-02-03 10:25:47 +0700

    fix bug

--- app/src/main/java/com/example/coinranking/presentation/main/CoinSearchAdapter.kt
+++ app/src/main/java/com/example/coinranking/presentation/main/CoinSearchAdapter.kt
@@ -5,20 +5,19 @@ import android.content.Context
 import android.graphics.drawable.PictureDrawable
 import android.net.Uri
 import android.os.Build
+import android.text.Editable
 import android.text.Html
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.paging.PagingDataAdapter
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
 import com.example.coinranking.data.CoinCoinsModel
 import com.example.coinranking.databinding.ItemListCoinBinding
-import com.example.coinranking.databinding.ItemListCoinSeparatorBinding
 import com.example.coinranking.presentation.helper.GlideApp
 import com.example.coinranking.presentation.helper.SvgSoftwareLayerSetter
+import org.xml.sax.XMLReader
 
 
 class CoinSearchAdapter(
@@ -28,47 +27,23 @@ class CoinSearchAdapter(
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private val inflater = LayoutInflater.from(mContext)
-    private val TYPE_SEPARATOR = 1
-    private val TYPE_NORMAL = 2
-    private lateinit var dataList:List<CoinCoinsModel>
+    private lateinit var dataList: List<CoinCoinsModel>
 
-    fun setDataList(newList: List<CoinCoinsModel>){
+    fun setDataList(newList: List<CoinCoinsModel>) {
         dataList = newList
     }
 
     override fun getItemCount(): Int {
-        return  dataList.size
-    }
-
-
-
-    override fun getItemViewType(position: Int): Int {
-        if (position % 5 == 4) {
-            return TYPE_SEPARATOR
-        } else {
-            return TYPE_NORMAL
-        }
+        return dataList.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return if (viewType == TYPE_NORMAL) {
-            val holder = ItemListCoinBinding.inflate(inflater, parent, false)
-            CoinViewHolder(holder)
-        } else {
-            val holder = ItemListCoinSeparatorBinding.inflate(inflater, parent, false)
-            SeparatorViewHolder(holder)
-        }
+        val holder = ItemListCoinBinding.inflate(inflater, parent, false)
+        return CoinViewHolder(holder)
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        when (holder.itemViewType) {
-            TYPE_SEPARATOR -> {
-                dataList[position].let { (holder as SeparatorViewHolder).binding(it) }
-            }
-            else -> {
-                dataList[position].let { (holder as CoinViewHolder).binding(it) }
-            }
-        }
+        dataList[position].let { (holder as CoinViewHolder).binding(it) }
     }
 
     inner class CoinViewHolder(private val binding: ItemListCoinBinding) :
@@ -93,39 +68,77 @@ class CoinSearchAdapter(
                 }
             }
             binding.tvName.text = model.name
+            val desc = model.description
+            desc?.let {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    binding.tvDesc.text = Html.fromHtml(desc, null, MyTagHandler())
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                binding.tvDesc.setText(Html.fromHtml(model.description, Html.FROM_HTML_MODE_COMPACT));
-            } else {
-                binding.tvDesc.setText(Html.fromHtml(model.description));
-            }
-        }
-    }
-
-    inner class SeparatorViewHolder(private val binding: ItemListCoinSeparatorBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        fun binding(model: CoinCoinsModel) {
-            if (model.iconType == "pixel") {
-                Glide.with(binding.root.context)
-                    .load(model.iconUrl)
-                    .diskCacheStrategy(DiskCacheStrategy.ALL)
-                    .into(binding.iv)
-            } else {
-                val requestBuilder = GlideApp.with(binding.root.context)
-                    .`as`(PictureDrawable::class.java)
-                    .transition(withCrossFade())
-                    .listener(SvgSoftwareLayerSetter())
-
-                model.iconUrl?.let {
-                    requestBuilder.diskCacheStrategy(DiskCacheStrategy.NONE)
-                        .load(Uri.parse(model.iconUrl))
-                        .into(binding.iv)
+                } else {
+                    binding.tvDesc.text = Html.fromHtml(model.description, null, MyTagHandler())
                 }
-
             }
-            binding.tvName.text = model.name
+
         }
     }
+
+    class MyTagHandler : Html.TagHandler {
+        var first = true
+        var parent: String? = null
+        var index = 1
+        override fun handleTag(opening: Boolean, tag: String, output: Editable, xmlReader: XMLReader?) {
+            if (tag == "ul") {
+                parent = "ul"
+            } else if (tag == "ol") {
+                parent = "ol"
+            }
+            if (tag == "li") {
+                if (parent == "ul") {
+                    first = if (first) {
+                        output.append("\n\t•")
+                        false
+                    } else {
+                        true
+                    }
+                } else {
+                    if (first) {
+                        output.append("\n\t$index. ")
+                        first = false
+                        index++
+                    } else {
+                        first = true
+                    }
+                }
+            }
+        }
+    }
+
+
+//    inner class SeparatorViewHolder(private val binding: ItemListCoinSeparatorBinding) :
+//        RecyclerView.ViewHolder(binding.root) {
+//        fun binding(model: CoinCoinsModel) {
+//            if (model.iconType == "pixel") {
+//                Glide.with(binding.root.context)
+//                    .load(model.iconUrl)
+//                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+//                    .into(binding.iv)
+//            } else {
+//                val requestBuilder = GlideApp.with(binding.root.context)
+//                    .`as`(PictureDrawable::class.java)
+//                    .transition(withCrossFade())
+//                    .listener(SvgSoftwareLayerSetter())
+//
+//                model.iconUrl?.let {
+//                    requestBuilder.diskCacheStrategy(DiskCacheStrategy.NONE)
+//                        .load(Uri.parse(model.iconUrl))
+//                        .into(binding.iv)
+//                }
+//
+//            }
+//            binding.tvName.text = model.name
+//        }
+//    }
+
+
 
 
 }
